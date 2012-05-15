ca0bfa64d5ce80f3bc008341cb52384513cdf1dd
Predrag Stanojevic <predrag.stanojevic@sonymobile.com>
2012-05-15 15:01:02 +0200

    Force closed at settings application when delete Misc files
    NullPointerException in MiscFilesHandler.java, missing check to
    ensure that dir.list() does not return null.
    
    Change-Id: I13aac19949826b385610fd1fee2befb411684023

--- src/com/android/settings/deviceinfo/MiscFilesHandler.java
+++ src/com/android/settings/deviceinfo/MiscFilesHandler.java
@@ -146,10 +146,12 @@ public class MiscFilesHandler extends ListActivity {
         private boolean deleteDir(File dir) {
             if (dir.isDirectory()) {
                 String[] children = dir.list();
-                for (int i=0; i < children.length; i++) {
-                    boolean success = deleteDir(new File(dir, children[i]));
-                    if (!success) {
-                        return false;
+                if (children != null) {
+                    for (int i=0; i < children.length; i++) {
+                        boolean success = deleteDir(new File(dir, children[i]));
+                        if (!success) {
+                            return false;
+                        }
                     }
                 }
             }
