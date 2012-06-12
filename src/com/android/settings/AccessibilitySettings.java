8e8ac6b134d135d16db36ba59a1ca7724d012049
Xingrun Chen <cxr514033970@gmail.com>
2012-06-12 14:15:39 +0800

    Don't direct to market if no markets installed
    The search intent cannot be resolved if there aren't any
    market apps installed, so it will cause Settings crashed by
    the ActivityNotFound exception.
    
    So we need to check if the market intent requst can be resolved.
    Then determine to notify user to direct the accessibility services
    to market to get TalkBack or not.
    
    Change-Id: I0f0d7b54eb5215e1a886315bb6816704384f6c59
    Signed-off-by: Roger Chen <cxr514033970@gmail.com>

--- src/com/android/settings/AccessibilitySettings.java
+++ src/com/android/settings/AccessibilitySettings.java
@@ -27,6 +27,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.pm.ServiceInfo;
 import android.content.res.Configuration;
@@ -181,9 +182,8 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
         super.onResume();
         loadInstalledServices();
         updateAllPreferences();
-        if (mServicesCategory.getPreference(0) == mNoServicesMessagePreference) {
-            offerInstallAccessibilitySerivceOnce();
-        }
+        offerInstallAccessibilitySerivceOnce();
+
         mSettingsPackageMonitor.register(getActivity(), false);
     }
 
@@ -484,6 +484,19 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
         if (mServicesCategory.getPreference(0) != mNoServicesMessagePreference) {
             return;
         }
+
+        String screenreaderMarketLink = SystemProperties.get(
+                SYSTEM_PROPERTY_MARKET_URL,
+                DEFAULT_SCREENREADER_MARKET_LINK);
+        Uri marketUri = Uri.parse(screenreaderMarketLink);
+        Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+
+        // No need to notify user to install TalkBack if the devices hasn't any market apps.
+        // Otherwise, Settings will throw the ActivityNotFound Exception after directed them to Market
+        if (getPackageManager().resolveActivity(marketIntent, 0) == null) {
+            return;
+        }
+
         SharedPreferences preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
         final boolean offerInstallService = !preferences.getBoolean(
                 KEY_INSTALL_ACCESSIBILITY_SERVICE_OFFERED_ONCE, false);
