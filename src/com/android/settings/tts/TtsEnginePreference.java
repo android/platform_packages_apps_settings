98267a66b2b5c630503a3515aae764ad78667ab6
Shuhrat Dehkanov <uzbmaster@gmail.com>
2012-10-03 12:24:07 +0900

    Grey out settings icon when it is in disabled state
    If engine is not active, its settings icon is disabled in
    Settings > Language & input > Text-to-speech output screen.
    Currently, settings icons for all TTS engines are shown at
    the same opacity. This fix dims settings icons of not-selected
    engines.
    Make sure that more than one TTS engine are installed on the
    target, for e.g., Google Text-to-speech Engine and Classic
    Text To Speech Engine (SVOX Classic TTS).
    
    Change-Id: Ifa7de79814a2f4a4aa021cd8621cbfab41655680
    Signed-off-by: Shuhrat Dehkanov <uzbmaster@gmail.com>

--- src/com/android/settings/tts/TtsEnginePreference.java
+++ src/com/android/settings/tts/TtsEnginePreference.java
@@ -79,6 +79,9 @@ public class TtsEnginePreference extends Preference {
     private RadioButton mRadioButton;
     private Intent mVoiceCheckData;
 
+    // TODO: Remove and reference this from resources
+    private static final float DISABLED_ALPHA = 0.4f;
+
     private final CompoundButton.OnCheckedChangeListener mRadioChangeListener =
         new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -135,6 +138,9 @@ public class TtsEnginePreference extends Preference {
         // Will be enabled only the engine has passed the voice check, and
         // is currently enabled.
         mSettingsIcon.setEnabled(isChecked && mVoiceCheckData != null);
+        if (!isChecked) {
+            mSettingsIcon.setAlpha(DISABLED_ALPHA);
+        }
         mSettingsIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -168,7 +174,12 @@ public class TtsEnginePreference extends Preference {
         // case mSettingsIcon && mRadioButton will be null. In this case
         // getView will set the right values.
         if (mSettingsIcon != null && mRadioButton != null) {
-            mSettingsIcon.setEnabled(mRadioButton.isChecked());
+            if (mRadioButton.isChecked()) {
+                mSettingsIcon.setEnabled(true);
+            } else {
+                mSettingsIcon.setEnabled(false);
+                mSettingsIcon.setAlpha(DISABLED_ALPHA);
+            }
         }
     }
 
