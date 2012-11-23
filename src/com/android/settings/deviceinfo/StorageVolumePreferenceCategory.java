04440288c16acc394ac6a0c6dfaef046498a4a38
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-23 16:42:11 +0700

    Settings: fix storage measurement for device without emulated sdcard
    StorageMeasurement: misc files should only be measured for internal
    storage only if it is the emulated storage, otherwise misc count
    for the primary storage will be erroneously duplicated on the
    internal storage count.
    
    StorageVolumePreferenceCategory:
      - Always create mMountTogglePreference to avoid null pointer
        exception for code that relies on it existing
      - Check if the keys exist before calculating totalValues to
        avoid null pointer exception on storage where media is not
        calculated
    
    Change-Id: Ib217f79ee562dffc514696fff038e58ed0dba7d9

--- src/com/android/settings/deviceinfo/StorageVolumePreferenceCategory.java
+++ src/com/android/settings/deviceinfo/StorageVolumePreferenceCategory.java
@@ -194,8 +194,9 @@ public class StorageVolumePreferenceCategory extends PreferenceCategory {
         }
 
         final boolean isRemovable = mVolume != null ? mVolume.isRemovable() : false;
+        // Always create the preference since many code rely on it existing
+        mMountTogglePreference = new Preference(context);
         if (isRemovable) {
-            mMountTogglePreference = new Preference(context);
             mMountTogglePreference.setTitle(R.string.sd_eject);
             mMountTogglePreference.setSummary(R.string.sd_eject_summary);
             addPreference(mMountTogglePreference);
@@ -310,7 +311,9 @@ public class StorageVolumePreferenceCategory extends PreferenceCategory {
     private static long totalValues(HashMap<String, Long> map, String... keys) {
         long total = 0;
         for (String key : keys) {
-            total += map.get(key);
+            if (map.containsKey(key)) {
+                total += map.get(key);
+            }
         }
         return total;
     }
