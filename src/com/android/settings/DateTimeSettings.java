b4f6c02747babe3dcd73fa65e6828c7b415c6333
Sungmin Choi <sungmin.choi@lge.com>
2013-03-01 21:20:46 +0900

    use API including start and end year in DatePickerDialog
    In case of using DatePickerDialog, user could select any date
    before 1970-01-01. Currently, Calendar is avaiable from 1970-01-01.
    So, use from 1970-01-01 to 2036-12-31 of DatePickerDialog.
    
    Steps to reproduce:
    1. Settings -> Date & time
    2. uncheck Automatic date & time
    3. Set Date
    4. choose any date before 1970 and Done
    
    Change-Id: I075b3fcaa86df1c314c85dd3dd85474a28b8c637

--- src/com/android/settings/DateTimeSettings.java
+++ src/com/android/settings/DateTimeSettings.java
@@ -243,7 +243,9 @@ public class DateTimeSettings extends SettingsPreferenceFragment
                 this,
                 calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH));
+                calendar.get(Calendar.DAY_OF_MONTH),
+                1970,
+                2036);
             break;
         }
         case DIALOG_TIMEPICKER: {
