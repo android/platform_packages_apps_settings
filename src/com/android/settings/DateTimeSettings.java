b47730c5de1d7fc7e7ee65050ba6d1a6ff6bcfad
Sungmin Choi <sungmin.choi@lge.com>
2013-03-01 21:20:46 +0900

    use date from 1970-01-01 to 2036-12-31 in DatePickerDialog
    In case of using DatePickerDialog, user could select any date
    before 1970-01-01. However, Calendar is available from 1970-01-01.
    As a result, user couldn't select date before 1970-01-01.
    So, use date from 1970-01-01 to 2036-12-31 of DatePickerDialog.
    
    Steps to reproduce:
    1. Settings -> Date & time
    2. uncheck Automatic date & time
    3. Set Date
    4. choose any date before 1970 and Done
    
    Change-Id: I075b3fcaa86df1c314c85dd3dd85474a28b8c637

--- src/com/android/settings/DateTimeSettings.java
+++ src/com/android/settings/DateTimeSettings.java
@@ -244,6 +244,14 @@ public class DateTimeSettings extends SettingsPreferenceFragment
                 calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH));
+            DatePickerDialog datePicker = (DatePickerDialog)d;
+            Calendar t = Calendar.getInstance();
+            t.clear();
+            t.set(1970, 0, 1);
+            datePicker.getDatePicker().setMinDate(t.getTimeInMillis());
+            t.clear();
+            t.set(2036, 11, 31);
+            datePicker.getDatePicker().setMaxDate(t.getTimeInMillis());
             break;
         }
         case DIALOG_TIMEPICKER: {
