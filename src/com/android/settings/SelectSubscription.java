10102f8a4888cc280dfe639a28345640f1da83d5
Rakesh Pallerla <rakesh@codeaurora.org>
2013-06-08 16:49:47 +0530

    Multisim: Add support for MSIM
    Add Multi SIM Settings screen in UI which is used to select
    Voice, SMS, Data preferences.
    Extend APN Settings, Security Settings, Wireless Settings
    for MSIM.
    
    Subscription specific for Mobile data, roaming and usage
     - Provide per subscription based support for both Mobile Data
       and Data roaming options. UI and functionality changes in:
       a. Settings -> Data Usage application
       b. Settings -> Mobile Networks menu
     - Provide per subscription based data usage information. UI and
       functionality changes in Settings -> Data Usage application.
    
    Add prompt support for SMS
     - Add prompt option in SMS multi sim settings.
     - New iterface API to get SMS prompt is enabled or not.
     - Disply prompt while sending SMS, if user selects
       prompt mode in multi sim settings.
    
    Extend CB "Operator info" feature to MSIM.
     - Add SUB information while broadcasting CB intent for MSIM.
     - Remove country specific checks for cellbroadcast Operator
       info for MSIM.
     - Move Operator info to About phone->Status->SelectSubscription
       ->SUB1/SUB2->Operator info in MSIM
    
    Change-Id: Icf5997dd6a6ba5e1de675cf5f4334c78c2c037f1

---
+++ src/com/android/settings/SelectSubscription.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.telephony.MSimTelephonyManager;
+import android.util.Log;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+public class SelectSubscription extends  TabActivity {
+
+    private static final String LOG_TAG = "SelectSubscription";
+    public static final String SUBSCRIPTION_KEY = "subscription";
+    public static final String PACKAGE = "PACKAGE";
+    public static final String TARGET_CLASS = "TARGET_CLASS";
+
+    private String[] tabLabel = {"SUB 1", "SUB 2", "SUB 3"};
+
+    private TabSpec subscriptionPref;
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    /*
+     * Activity class methods
+     */
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        log("Creating activity");
+
+        setContentView(R.layout.select_subscription);
+
+        TabHost tabHost = getTabHost();
+
+        Intent intent =  getIntent();
+        String pkg = intent.getStringExtra(PACKAGE);
+        String targetClass = intent.getStringExtra(TARGET_CLASS);
+
+        int numPhones = MSimTelephonyManager.getDefault().getPhoneCount();
+
+        for (int i = 0; i < numPhones; i++) {
+            subscriptionPref = tabHost.newTabSpec(tabLabel[i]);
+            subscriptionPref.setIndicator(tabLabel[i]);
+            intent = new Intent().setClassName(pkg, targetClass)
+                    .setAction(intent.getAction()).putExtra(SUBSCRIPTION_KEY, i);
+            subscriptionPref.setContent(intent);
+            tabHost.addTab(subscriptionPref);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private static void log(String msg) {
+        Log.d(LOG_TAG, msg);
+    }
+}
