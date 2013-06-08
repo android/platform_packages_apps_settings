342154e4cc394c49b825a331a168eb5b7ad6ec5d
Rakesh Pallerla <rakesh@codeaurora.org>
2013-06-08 16:49:47 +0530

    Multisim: Add support for MSIM
    Add Multi SIM Settings screen in UI which is used to select
    Voice, SMS, Data preferences.
    Extend APN Settings, Security Settings, Wireless Settings
    for MSIM.
    
    Subscription specific for Mobile data, roaming and usage
     - Provide per subscription based support for both Mobile Data
       and Data roaming options. UI and functionality changes in:
       a. Settings -> Data Usage application
       b. Settings -> Mobile Networks menu
     - Provide per subscription based data usage information. UI and
       functionality changes in Settings -> Data Usage application.
    
    Add prompt support for SMS
     - Add prompt option in SMS multi sim settings.
     - New iterface API to get SMS prompt is enabled or not.
     - Disply prompt while sending SMS, if user selects
       prompt mode in multi sim settings.
    
    Extend CB "Operator info" feature to MSIM.
     - Add SUB information while broadcasting CB intent for MSIM.
     - Remove country specific checks for cellbroadcast Operator
       info for MSIM.
     - Move Operator info to About phone->Status->SelectSubscription
       ->SUB1/SUB2->Operator info in MSIM
    
    Change-Id: Icf5997dd6a6ba5e1de675cf5f4334c78c2c037f1

--- src/com/android/settings/IccLockSettings.java
+++ src/com/android/settings/IccLockSettings.java
@@ -35,6 +35,7 @@ import android.widget.Toast;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneFactory;
 import com.android.internal.telephony.TelephonyIntents;
+import com.codeaurora.telephony.msim.MSimPhoneFactory;
 
 /**
  * Implements the preference screen to enable/disable ICC lock and
@@ -182,7 +183,11 @@ public class IccLockSettings extends PreferenceActivity
         // Don't need any changes to be remembered
         getPreferenceScreen().setPersistent(false);
 
-        mPhone = PhoneFactory.getDefaultPhone();
+        Intent intent = getIntent();
+        int subscription = intent.getIntExtra(SelectSubscription.SUBSCRIPTION_KEY,
+                MSimPhoneFactory.getDefaultSubscription());
+        // Use the right phone based on the subscription selected.
+        mPhone = MSimPhoneFactory.getPhone(subscription);
         mRes = getResources();
         updatePreferences();
     }
