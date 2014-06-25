91d636d066f0c0a18f36d418f14a2497633b4be9
yuka suzuki <yuka.x.suzuki@sonymobile.com>
2014-06-25 17:14:08 +0900

    Location confirmation dialog is displayed when Location is on
    When config_showLocationDialog is true, the location confirmation
    dialog is displayed when the user turns the location setting on.
    
    Fixes: 28057031
    Test: manual - turn the location setting on.
    
    Change-Id: Ifd61edb9a194207bfd02ea650ca28a020fd31c65

--- src/com/android/settings/location/LocationSettingsBase.java
+++ src/com/android/settings/location/LocationSettingsBase.java
@@ -38,7 +38,10 @@ public abstract class LocationSettingsBase extends SettingsPreferenceFragment {
     private static final String MODE_CHANGING_ACTION =
             "com.android.settings.location.MODE_CHANGING";
     private static final String CURRENT_MODE_KEY = "CURRENT_MODE";
-    private static final String NEW_MODE_KEY = "NEW_MODE";
+    static final String NEW_MODE_KEY = "NEW_MODE";
+
+    static final String PREFERENCE_FILE_NAME = "location_settings";
+    static final String KEY_NEVER_ASK = "never_ask";
 
     private int mCurrentMode;
     private BroadcastReceiver mReceiver;
@@ -115,8 +118,10 @@ public abstract class LocationSettingsBase extends SettingsPreferenceFragment {
             return;
         }
 
-        updateLocationMode(context, mCurrentMode, mode);
-        refreshLocationMode();
+        Intent intent = new Intent(context, LocationReceiver.class)
+                .putExtra(NEW_MODE_KEY, mode)
+                .addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        context.sendBroadcast(intent, android.Manifest.permission.WRITE_SECURE_SETTINGS);
     }
 
     public void refreshLocationMode() {
