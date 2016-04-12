d5f6d33b19a6403aee635a4533a33aef1305d267
Cheng-Hung Ko <ch.ko@mediatek.com>
2016-04-12 17:18:12 +0800

    Test Test Test for upstream
    Change-Id: Ib105804f45fc20effca5ea004c2d7de67df1e4ef

--- Android.mk
+++ Android.mk
@@ -29,6 +29,8 @@ include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
 
+# test for upstream
+
 # Use the following include to make our test apk.
 ifeq (,$(ONE_SHOT_MAKEFILE))
 include $(call all-makefiles-under,$(LOCAL_PATH))
