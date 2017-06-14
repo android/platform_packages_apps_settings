180a1d724c01862a98f84f373d3296bb8e0d060f
PMS22 <prathams99@rediff.com>
2017-06-14 07:10:15 +0000

    Settings: Correctly align cursor in Settings
    Steps to Reproduce:
    - Click on Density Settings (DPI Settings) / Renaming BT Device / Renaming SIM Settings
    - EditText Dialog pops up
    - The cursor is at the leftmost side of the text
    - Logically Cursor should be at rightmost side which can be aligned grabbing text length
    
    Screenshots:
    Before:
    http://imgur.com/a/o4vkr
    http://imgur.com/a/6Qext
    
    After:
    http://imgur.com/a/fuxqT
    http://imgur.com/a/vm2WX
    
    Bug: 65185652
    
    Test: Manual test
    
    RunSettingsRoboTests: OK (47 tests)
    
    Change-Id: Id00308e7e019434edad4afab56cf4719be0d0d54
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- tests/robotests/src/com/android/settings/UtilsTest.java
+++ tests/robotests/src/com/android/settings/UtilsTest.java
@@ -23,6 +23,8 @@ import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.format.DateUtils;
 import android.text.style.TtsSpan;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -165,5 +167,16 @@ public class UtilsTest {
         info.enabled = false;
 
         assertThat(Utils.getInstallationStatus(info)).isEqualTo(R.string.disabled);
+    }
+
+    @Test
+    public void testSetEditTextCursorPosition_shouldGetExpectedEditTextLenght() {
+        EditText editText = new EditText(mContext);
+        CharSequence text = "test";
+        editText.setText(text, TextView.BufferType.EDITABLE);
+        int length = editText.getText().length();
+        Utils.setEditTextCursorPosition(editText);
+
+        assertThat(editText.getSelectionEnd()).isEqualTo(length);
     }
 }
