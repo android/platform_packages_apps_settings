7ccdc5b24876b8c0d234f935604531d4da2d28ba
Hemant Gupta <hemantg@codeaurora.org>
2017-07-04 19:56:35 +0530

    Bluetooth: Broadcast intent with foreground priority
    Usecase:
    1) Select a file to be transferred via BT to remote device.
    2) Select the remote device and wait for file transfer to start
    
    Observation:
    File transfer does not start and file sending fails.
    
    Root Cause:
    android.bluetooth.devicepicker.action.DEVICE_SELECTED intent is not
    received in BluetoothOppReceiver leading to transfer not getting started.
    
    Fix:
    Broadcast intent with Intent.FLAG_RECEIVER_FOREGROUND to ensure
    it is received in timely manner by OPP.
    
    Test: OPP transfer works fine everytime.
    
    Bug: 63290527
    Change-Id: Ie18d7fb4f8612530bac4de6f15d018301597bc44

--- src/com/android/settings/bluetooth/DevicePickerFragment.java
+++ src/com/android/settings/bluetooth/DevicePickerFragment.java
@@ -161,6 +161,7 @@ public final class DevicePickerFragment extends DeviceListPreferenceFragment {
         if (mLaunchPackage != null && mLaunchClass != null) {
             intent.setClassName(mLaunchPackage, mLaunchClass);
         }
+        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
         getActivity().sendBroadcast(intent);
     }
 }
