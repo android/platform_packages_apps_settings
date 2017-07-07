f17f429dcd4031f5fa95407b6978897d056732bf
Hao Zhao <hao.xa.zhao@sony.com>
2017-07-07 11:00:38 +0800

    Don't show the package which has MANAGE_NOTIFICATIONS permisson
    If the target package has MANAGE_NOTIFICATIONS permission,
    NotificationManager#isNotificationPolicyAccessGrantedForPackage()
    always returns true. MANAGE_NOTIFICATIONS is a fixed permisson.
    It can't be revoked.
    
    Bug: xxxxxxxxx
    Test: robotests
    Change-Id: I924042bb147917d76349618d38b6f4dd7dd8db21

--- tests/robotests/src/com/android/settings/notification/ZenAccessSettingsTest.java
+++ tests/robotests/src/com/android/settings/notification/ZenAccessSettingsTest.java
@@ -16,11 +16,17 @@
 
 package com.android.settings.notification;
 
+import static org.junit.Assert.assertFalse;
+
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.testutils.FakeFeatureFactory;
@@ -33,6 +39,10 @@ import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @RunWith(SettingsRobolectricTestRunner.class)
 public class ZenAccessSettingsTest {
 
@@ -40,12 +50,14 @@ public class ZenAccessSettingsTest {
     private Context mContext;
 
     private FakeFeatureFactory mFeatureFactory;
+    private ZenAccessSettings mFragment;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
         mFeatureFactory = FakeFeatureFactory.setupForTest();
+        mFragment = spy(new ZenAccessSettings());
     }
 
     @Test
@@ -59,5 +71,40 @@ public class ZenAccessSettingsTest {
         verify(mFeatureFactory.metricsFeatureProvider).action(any(Context.class),
                 eq(MetricsProto.MetricsEvent.APP_SPECIAL_PERMISSION_DND_DENY),
                 eq("app"));
+    }
+
+    @Test
+    public void removeFixedNotificationPolicyAccess() {
+        // Set up 3 installed apps, in which 1 is hidden module
+        final List<ApplicationInfo> apps = new ArrayList<>();
+        apps.add(createApplicationInfo("test.package.1"));
+        apps.add(createApplicationInfo("test.hidden.module.2"));
+        apps.add(createApplicationInfo("test.package.3"));
+
+        doReturn(true).when(mFragment).isManageNotificationsPermissionGranted("test.hidden.module.2");
+        doReturn(false).when(mFragment).isManageNotificationsPermissionGranted("test.package.1");
+        doReturn(false).when(mFragment).isManageNotificationsPermissionGranted("test.package.3");
+        mFragment.removeFixedNotificationPolicyAccess(apps);
+
+        assertFalse(isAppInApplicationList("test.hidden.module.2", apps));
+    }
+
+    private ApplicationInfo createApplicationInfo(String packageName) {
+        ApplicationInfo appInfo = new ApplicationInfo();
+        appInfo.sourceDir = "foo";
+        appInfo.flags |= ApplicationInfo.FLAG_INSTALLED;
+        appInfo.storageUuid = UUID.randomUUID();
+        appInfo.packageName = packageName;
+        return appInfo;
+    }
+
+    private boolean isAppInApplicationList(
+            String packageName, List<ApplicationInfo> applicationList) {
+        for (ApplicationInfo appInfo : applicationList) {
+            if (appInfo.packageName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
