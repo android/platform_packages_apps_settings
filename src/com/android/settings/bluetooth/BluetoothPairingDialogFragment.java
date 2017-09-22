9a8b5efa68facf9a776d2879fffbd67a4cf02ec3
Hemant Gupta <hemantg@codeaurora.org>
2017-09-22 12:24:14 +0530

    Bluetooth: Handle Contact share UI checkbox issue
    Step by step:
    1. Pair and connect with BTHS (e.g. SBH50).
    2. Click the settings icon of paired device, and check "Use for" items.
    -----------------------------------------------------------------
    Expected Result:
    After step 2, "Contact sharing" should not be checked.
    
    Actual Result:
    After step 2, "Contact sharing" is checked.
    
    Fix:
    Set phone book permission as REJECTED if class of Device is
    not AUDIO_VIDEO_HADNFREE
    
    Test: Checked above usecase and issue is not observed
    
    Bug: 64372703
    Change-Id: I178a09dd9e76d67c461c4b04d9b2ee0078ed24c9

--- src/com/android/settings/bluetooth/BluetoothPairingDialogFragment.java
+++ src/com/android/settings/bluetooth/BluetoothPairingDialogFragment.java
@@ -212,6 +212,7 @@ public class BluetoothPairingDialogFragment extends InstrumentedDialogFragment i
 
         contactSharing.setVisibility(mPairingController.isProfileReady()
                 ? View.GONE : View.VISIBLE);
+        mPairingController.setContactSharingState();
         contactSharing.setOnCheckedChangeListener(mPairingController);
         contactSharing.setChecked(mPairingController.getContactSharingState());
 
@@ -302,6 +303,7 @@ public class BluetoothPairingDialogFragment extends InstrumentedDialogFragment i
 
         contactSharing.setVisibility(
                 mPairingController.isProfileReady() ? View.GONE : View.VISIBLE);
+        mPairingController.setContactSharingState();
         contactSharing.setChecked(mPairingController.getContactSharingState());
         contactSharing.setOnCheckedChangeListener(mPairingController);
 
