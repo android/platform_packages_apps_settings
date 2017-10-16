2b66b642e4a216339844c2657bc2e10fbaaabcd9
jianzhou <jianzhou@codeaurora.org>
2017-10-16 15:55:09 +0800

    Fix Wi-Fi list adds same AP repeatedly
    Bug: 67831343
    Change-Id: I5502ed50a0b5f3614fd5ebc280fae6054be256d5

--- src/com/android/settings/wifi/WifiSettings.java
+++ src/com/android/settings/wifi/WifiSettings.java
@@ -734,10 +734,7 @@ public class WifiSettings extends RestrictedSettingsFragment
             AccessPoint accessPoint = accessPoints.get(index);
             // Ignore access points that are out of range.
             if (accessPoint.isReachable()) {
-                String key = accessPoint.getBssid();
-                if (TextUtils.isEmpty(key)) {
-                    key = accessPoint.getSsidStr();
-                }
+                String key = generateKey(accessPoint);
                 hasAvailableAccessPoints = true;
                 LongPressAccessPointPreference pref =
                         (LongPressAccessPointPreference) getCachedPreference(key);
@@ -779,6 +776,18 @@ public class WifiSettings extends RestrictedSettingsFragment
         }
     }
 
+    private String generateKey(AccessPoint accessPoint) {
+        StringBuilder key = new StringBuilder();
+        String bssid = accessPoint.getBssid();
+        if (TextUtils.isEmpty(bssid)) {
+            key.append(accessPoint.getSsidStr());
+        } else {
+            key.append(bssid);
+        }
+        key.append(',').append(accessPoint.getSecurity());
+        return key.toString();
+    }
+
     @NonNull
     private LongPressAccessPointPreference createLongPressActionPointPreference(
             AccessPoint accessPoint) {
@@ -810,10 +819,11 @@ public class WifiSettings extends RestrictedSettingsFragment
         }
 
         // Is the previous currently connected SSID different from the new one?
-        if (!((AccessPointPreference)
-                mConnectedAccessPointPreferenceCategory.getPreference(0))
-                        .getAccessPoint().getSsidStr().equals(
-                                connectedAp.getSsidStr())) {
+        AccessPointPreference preference = (AccessPointPreference)
+            (mConnectedAccessPointPreferenceCategory.getPreference(0));
+        // The AccessPoints need to be the same reference to ensure that updates are reflected
+        // in the UI.
+        if (preference.getAccessPoint() != connectedAp) {
             removeConnectedAccessPointPreference();
             addConnectedAccessPointPreference(connectedAp);
             return true;
