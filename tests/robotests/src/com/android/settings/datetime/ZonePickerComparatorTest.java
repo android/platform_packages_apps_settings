f282db5cf80a08c38decd17a2561a805fd6432ae
tiansiming <tiansiming@xiaomi.com>
2017-10-30 21:39:48 +0800

    Modify Comparator in ZonePicker
    The previous method "compareTo" isn't locale-sensitive, it cannot
    sort the String in some other languages. So Collator is recommended
    to be applyed here.
    
    Bug: Sort doesn't work in some languages.
    Test: Switch the system language to Chinaese and enter ZonePicker.
    
    Change-Id: Id107ab938cceefc77f9fb6918a0445fc92a0fcb7
    Signed-off-by: tiansiming <tiansiming@xiaomi.com>

---
+++ tests/robotests/src/com/android/settings/datetime/ZonePickerComparatorTest.java
@@ -0,0 +1,82 @@
+package com.android.settings.datetime;
+
+import com.android.settings.datetime.ZonePicker;
+import com.android.settings.testutils.SettingsRobolectricTestRunner;
+import com.android.settings.TestConfig;
+import com.android.settingslib.datetime.ZoneGetter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(SettingsRobolectricTestRunner.class)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+public class ZonePickerComparatorTest {
+
+    private static final int TESTNUM = 5;
+    private String mSortKey;
+    private List<Map<String, Object>> mTestZonesList;
+    private final String[] mTestDisplayName =
+            new String[]{"中国标准时间", "纽约", "悉尼", "伦敦", "洛杉矶"};
+    private final String[] mOrderedDisplayName =
+            new String[]{"伦敦", "洛杉矶", "纽约", "悉尼", "中国标准时间"};
+
+    private List<Map<String, Object>> getMockZonesList() {
+        List<Map<String, Object>> zones = new ArrayList<>();
+        TimeZone tz = TimeZone.getDefault();
+        for (int i = 0; i < TESTNUM; i++) {
+            zones.add(createMockDisplayEntry(tz, "GMT+08:00", mTestDisplayName[i], TESTNUM - i - 1));
+        }
+        return zones;
+    }
+
+    private Map<String, Object> createMockDisplayEntry(
+            TimeZone tz, CharSequence gmtOffsetText, CharSequence displayName, int offsetMillis) {
+        Map<String, Object> map = new HashMap<>();
+        map.put(ZoneGetter.KEY_ID, tz.getID());
+        map.put(ZoneGetter.KEY_DISPLAYNAME, displayName.toString());
+        map.put(ZoneGetter.KEY_DISPLAY_LABEL, displayName);
+        map.put(ZoneGetter.KEY_GMT, gmtOffsetText.toString());
+        map.put(ZoneGetter.KEY_OFFSET_LABEL, gmtOffsetText);
+        map.put(ZoneGetter.KEY_OFFSET, offsetMillis);
+        return map;
+    }
+
+    @Before
+    public void getTestList() {
+        mTestZonesList = getMockZonesList();
+    }
+    
+    @Test
+    public void testComparator_sortString() {
+        mSortKey = ZoneGetter.KEY_DISPLAY_LABEL;
+        final ZonePicker.MyComparator comparator = new ZonePicker.MyComparator(mSortKey);
+        assertThat(comparator).isNotNull();
+        Collections.sort(mTestZonesList, comparator);
+        for (int i = 0; i < TESTNUM; i++) {
+            assertThat(mTestZonesList.get(i).get(mSortKey).toString())
+                    .isEqualTo(mOrderedDisplayName[i]);
+        }
+    }
+
+    @Test
+    public void testComparator_sortInteger() {
+        mSortKey = ZoneGetter.KEY_OFFSET;
+        final ZonePicker.MyComparator comparator = new ZonePicker.MyComparator(mSortKey);   
+        assertThat(comparator).isNotNull();
+        Collections.sort(mTestZonesList, comparator);
+        for (int i = 0; i < TESTNUM; i++) {
+            assertThat(mTestZonesList.get(i).get(mSortKey)).isEqualTo(i);
+        }
+    }
+}
