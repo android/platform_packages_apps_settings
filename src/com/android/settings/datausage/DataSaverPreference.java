90120915d7ca6e344a65dabc1f6802c2e44fc692
Jin Dong <jin.x.dong@sony.com>
2017-12-22 17:49:23 +0800

    Fix memory leak of DataSaverPreference
    When DataSaverPreference is shown repeatedly, memory leak happens
    since DataSaverBackend.Listener is not removed on onDetached().
    
    TEST: repeat enter and exit "Data usage" for
    several times and check if the count of activity
    is not increasing.
    
    Bug: 72581115
    Change-Id: Ic4a72de049bb87f1e522fe46dd0fb4bb58ed65a5

--- src/com/android/settings/datausage/DataSaverPreference.java
+++ src/com/android/settings/datausage/DataSaverPreference.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2018 Sony Mobile Communications Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -10,6 +11,9 @@
  * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
+ *
+ * NOTE: This file has been modified by Sony Mobile Communications Inc.
+ * Modifications are licensed under the License.
  */
 
 package com.android.settings.datausage;
@@ -37,7 +41,7 @@ public class DataSaverPreference extends Preference implements DataSaverBackend.
     @Override
     public void onDetached() {
         super.onDetached();
-        mDataSaverBackend.addListener(this);
+        mDataSaverBackend.remListener(this);
     }
 
     @Override
