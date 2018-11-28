25253eac8a27a0f5f6eccc06355f03b5cc379421
Joshua Duong <joshuaduong@google.com>
2018-11-28 11:11:32 -0800

    [adb-wireless] Add fragment for pairing new devices.
    Change-Id: I54fd247719fbbd4f3d14b20ee0a9ea224cab1034

---
+++ src/com/android/settings/development/AdbPairingDeviceFragment.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.development;
+
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdbPairingDeviceFragment extends DashboardFragment {
+    private final String TAG = this.getClass().getSimpleName();
+    private AdbPairingDeviceController mAdbPairingDeviceController;
+    private String mDeviceName;
+
+    public static final int PAIRING_DEVICE_DIALOG_ID = 1;
+    public static final int PAIRING_DEVICE_FAILED_DIALOG_ID = 2;
+
+    public AdbPairingDeviceFragment() {
+        super();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        Bundle bundle = getArguments();
+        CharSequence res = bundle.getCharSequence(WirelessDebugging.DEVICE_NAME_EXTRA);
+        if (res != null) {
+            mDeviceName = res.toString();
+        }
+        super.onAttach(context);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return 0;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.adb_pairing_device_fragment;
+    }
+
+    public void showDevicePairingDialog() {
+        showDialog(PAIRING_DEVICE_DIALOG_ID);
+    }
+
+    @Override
+    public Dialog onCreateDialog(int dialogId) {
+        Dialog d = mAdbPairingDeviceController.createDialog(dialogId);
+        if (d != null) {
+            return d;
+        }
+        return super.onCreateDialog(dialogId);
+    }
+
+
+
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        return MetricsEvent.ADB_WIRELESS_DEVICE_PAIRING_DIALOG;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        mAdbPairingDeviceController = AdbPairingDeviceController.newInstance(
+                mDeviceName,
+                context,
+                this,
+                getSettingsLifecycle());
+
+        controllers.add(mAdbPairingDeviceController);
+
+        return controllers;
+    }
+}
