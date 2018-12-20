822a15d548e7b5f85bf69ed548998a4eff61d428
jin.dong <jin.x.dong@sony.com>
2018-12-20 11:15:25 +0800

    Add empty PreferenceCategory and customizable order
    - Add "android:order" for all items and categories:
        about_legal.xml
        battery_saver_settings.xml
        gestures.xml
        wifi_configure_settings.xml
    
    - Add PreferenceCategory and order for items and categories:
        connected_devices_advanced.xml - x5 categories
        display_settings.xml - x5 categories
        power_usage_summary.xml - x5 categories
        sound_settings.xml - x5 categories
        security_lockscreen_settings.xml - x5 categories
    
    - Add "android:order" for items and categories by RRO:
        connected_devices_advanced.xml
        display_settings.xml
        security_lockscreen_settings.xml
    
    - Change to display the divider by config:
        sound_settings.xml
        power_usage_summary.xml
    
    Change-Id: I8409c42a856e3cd08c7572b5d3bd9e0455f3b087

---
+++ src/com/android/settings/emptycategory/LockScreenSettingsEmptyCategoryDPreferenceController.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.settings.emptycategory;
+
+import android.content.Context;
+
+import com.android.settings.core.BasePreferenceController;
+import com.android.settings.R;
+
+public class LockScreenSettingsEmptyCategoryDPreferenceController
+        extends BasePreferenceController {
+
+    public LockScreenSettingsEmptyCategoryDPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    @AvailabilityStatus
+    public int getAvailabilityStatus() {
+        return mContext.getResources().getBoolean(
+                R.bool.config_lockscreen_settings_empty_category_d_visible)
+                ? AVAILABLE_UNSEARCHABLE
+                : UNSUPPORTED_ON_DEVICE;
+    }
+}
