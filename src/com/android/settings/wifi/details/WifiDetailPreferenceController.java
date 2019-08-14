f7b0403326ff482c05731d55e2095ee662bcf237
lucaslin <lucaslin@google.com>
2019-08-14 18:21:01 +0800

    Better notification to user when DNS-over-TLS is blocked
    Provide a more better notification to let users know that device
    has no internet is because it really doesn't have internet access
    or blocked by private DNS resolution failed.
    
    Bug: 113242081
    Test: make -j44 RunSettingsRoboTests \
          ROBOTEST_FILTER=WifiDetailPreferenceControllerTest
    
    Change-Id: I44a7e51cf76dc3f2a6b9583f226af6649f9b1f5f

--- src/com/android/settings/wifi/details/WifiDetailPreferenceController.java
+++ src/com/android/settings/wifi/details/WifiDetailPreferenceController.java
@@ -194,13 +194,14 @@ public class WifiDetailPreferenceController extends AbstractPreferenceController
         @Override
         public void onCapabilitiesChanged(Network network, NetworkCapabilities nc) {
             // If the network just validated or lost Internet access or detected partial internet
-            // connectivity, refresh network state. Don't do this on every NetworkCapabilities
-            // change because refreshNetworkState sends IPCs to the system server from the UI
-            // thread, which can cause jank.
+            // connectivity or private dns is broken, refresh network state. Don't do this on
+            // every NetworkCapabilities change because refreshNetworkState sends IPCs to the
+            // system server from the UI thread, which can cause jank.
             if (network.equals(mNetwork) && !nc.equals(mNetworkCapabilities)) {
                 if (hasCapabilityChanged(nc, NET_CAPABILITY_VALIDATED)
                         || hasCapabilityChanged(nc, NET_CAPABILITY_CAPTIVE_PORTAL)
-                        || hasCapabilityChanged(nc, NET_CAPABILITY_PARTIAL_CONNECTIVITY)) {
+                        || hasCapabilityChanged(nc, NET_CAPABILITY_PARTIAL_CONNECTIVITY)
+                        || mNetworkCapabilities.isPrivateDnsBroken() != nc.isPrivateDnsBroken()) {
                     refreshNetworkState();
                 }
                 mNetworkCapabilities = nc;
