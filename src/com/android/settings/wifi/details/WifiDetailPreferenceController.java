9be5e0bce9ff2e764f43d699178deef5a6944d69
lucaslin <lucaslin@google.com>
2019-08-16 09:42:36 +0800

    Improve the wifi description for the case of private DNS broken
    Currently, users don't know if the wifi description shows
    "No internet" means it really doesn't have internet access or no
    internet is because of private DNS resolution failed. So, Add a
    new description for wifi when private DNS is broken.
    
    Bug: 113242081
    Test: make -j44 RunSettingsRoboTests \
          ROBOTEST_FILTER=WifiDetailPreferenceControllerTest
    
    Change-Id: I44a7e51cf76dc3f2a6b9583f226af6649f9b1f5f

--- src/com/android/settings/wifi/details/WifiDetailPreferenceController.java
+++ src/com/android/settings/wifi/details/WifiDetailPreferenceController.java
@@ -191,14 +191,24 @@ public class WifiDetailPreferenceController extends AbstractPreferenceController
             return mNetworkCapabilities.hasCapability(cap) != nc.hasCapability(cap);
         }
 
+        private boolean prviateDnsStatusChanged(NetworkCapabilities nc) {
+            // If this is the first time we get NetworkCapabilities, report that something changed.
+            if (mNetworkCapabilities == null) {
+                return true;
+            }
+
+            return mNetworkCapabilities.isPrivateDnsBroken() != nc.isPrivateDnsBroken();
+        }
+
         @Override
         public void onCapabilitiesChanged(Network network, NetworkCapabilities nc) {
             // If the network just validated or lost Internet access or detected partial internet
-            // connectivity, refresh network state. Don't do this on every NetworkCapabilities
-            // change because refreshNetworkState sends IPCs to the system server from the UI
-            // thread, which can cause jank.
+            // connectivity or private dns is broken, refresh network state. Don't do this on
+            // every NetworkCapabilities change because refreshNetworkState sends IPCs to the
+            // system server from the UI thread, which can cause jank.
             if (network.equals(mNetwork) && !nc.equals(mNetworkCapabilities)) {
-                if (hasCapabilityChanged(nc, NET_CAPABILITY_VALIDATED)
+                if (prviateDnsStatusChanged(nc)
+                        || hasCapabilityChanged(nc, NET_CAPABILITY_VALIDATED)
                         || hasCapabilityChanged(nc, NET_CAPABILITY_CAPTIVE_PORTAL)
                         || hasCapabilityChanged(nc, NET_CAPABILITY_PARTIAL_CONNECTIVITY)) {
                     refreshNetworkState();
