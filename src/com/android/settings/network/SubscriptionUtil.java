010f5ac8a3eb6b196d5a6cec81168147be5e8e62
Bonian Chen <bonianchen@google.com>
2020-01-02 18:59:24 +0800

    [Settings] Replacing accessing of PhoneConstants$DataState
    1. Replaced by PhoneStateListner#onPreciseDataConnectionStateChanged().
    2. Remove usage of hiddepn API SubscriptionManager#getPhoneId
    
    Bug: 147080692
    Test: manual
    Change-Id: I6e7c9910ed63e027fc02c1835853175745b31b66

--- src/com/android/settings/network/SubscriptionUtil.java
+++ src/com/android/settings/network/SubscriptionUtil.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.network;
 
+import static android.telephony.SubscriptionManager.INVALID_SIM_SLOT_INDEX;
 import static android.telephony.UiccSlotInfo.CARD_STATE_INFO_PRESENT;
 
 import static com.android.internal.util.CollectionUtils.emptyIfNull;
@@ -112,5 +113,28 @@ public class SubscriptionUtil {
             return name.toString();
         }
         return "";
+    }
+
+    /**
+     * Whether Settings should show a "Use SIM" toggle in pSIM detailed page.
+     */
+    public static boolean showToggleForPhysicalSim(SubscriptionManager subMgr) {
+        return subMgr.canDisablePhysicalSubscription();
+    }
+
+    /**
+     * Get phoneId or logical slot index for a subId if active, or INVALID_PHONE_INDEX if inactive.
+     */
+    public static int getPhoneId(Context context, int subId) {
+        SubscriptionManager subManager = (SubscriptionManager)
+                context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
+        if (subManager == null) {
+            return INVALID_SIM_SLOT_INDEX;
+        }
+        SubscriptionInfo info = subManager.getActiveSubscriptionInfo(subId);
+        if (info == null) {
+            return INVALID_SIM_SLOT_INDEX;
+        }
+        return info.getSimSlotIndex();
     }
 }
