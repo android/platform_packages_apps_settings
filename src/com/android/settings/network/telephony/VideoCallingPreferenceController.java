c306ee796740c67f2745ced2b2a1d5c9abaf99ab
Jason Chiu <chiujason@google.com>
2020-04-09 14:29:58 +0800

    Fix indexing error of mobile related controllers
    There were several errors in indexing mobile related sliceable
    controllers, which led them not able to display on Panel and as inline
    controls in search result.
    
    It's because indexing mechanism is running in a worker thread and trying
    to construct each controller instance, but the failed controllers'
    constructors need to initilize something in the main thread.
    
    - Give the main looper to the classes which only can be initialized in
      the main thread to fix the indexing.
    - Guard null pointer exception in SlicesIndexer after fixing the
      indexing.
    - Use onStart/onStop in AirplaneModePreferenceController to start/stop
      listener.
    
    Fixes: 149720345
    Test: robotest
    Merged-In: Ibe5a8d6cc713eeddf26eceaabc05e6d1faa45507
    Change-Id: I996062326399c97934a109ee09a1ba7caf72dc22

--- src/com/android/settings/network/telephony/VideoCallingPreferenceController.java
+++ src/com/android/settings/network/telephony/VideoCallingPreferenceController.java
@@ -150,8 +150,8 @@ public class VideoCallingPreferenceController extends TelephonyTogglePreferenceC
 
     private class PhoneCallStateListener extends PhoneStateListener {
 
-        public PhoneCallStateListener(Looper looper) {
-            super(looper);
+        PhoneCallStateListener() {
+            super(Looper.getMainLooper());
         }
 
         @Override
