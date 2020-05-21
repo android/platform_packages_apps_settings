e6318946cd7272e824fedb0cb68b4ae3cbb1a31d
Felix <google@ix5.org>
2020-05-22 00:16:14 +0200

    tests: Add CameraLiftTrigger robotests
    Test CameraLiftTriggerSettings and
    CameraLiftTriggerPreferenceController.
    
    Mock context for config_cameraLiftTriggerSensorType to cycle
    through valid and invalid type integers.
    
    Test: m SettingsRoboTests
    Test: croot && m RunSettingsRoboTests \
        ROBOTEST_FILTER=CameraLiftTrigger
    
    > RunSettingsRoboTests:
    > RunSettingsRoboTests: Time: 11.616
    > RunSettingsRoboTests:
    > RunSettingsRoboTests: OK (13 tests)
    > RunSettingsRoboTests:
    
    Signed-off-by: Felix <google@ix5.org>
    Change-Id: I62663f087fa905244fb035a47f02935ad70cc72f

---
+++ tests/robotests/src/com/android/settings/gestures/CameraLiftTriggerSettingsTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.provider.SearchIndexableResource;
+
+import com.android.settings.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import java.util.List;
+
+@RunWith(RobolectricTestRunner.class)
+public class CameraLiftTriggerSettingsTest {
+
+    private CameraLiftTriggerSettings mSettings;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        mSettings = new CameraLiftTriggerSettings();
+    }
+
+    @Test
+    public void testGetPreferenceScreenResId() {
+        assertThat(mSettings.getPreferenceScreenResId())
+                .isEqualTo(R.xml.camera_lift_trigger_settings);
+    }
+
+    @Test
+    public void testSearchIndexProvider_shouldIndexResource() {
+        final List<SearchIndexableResource> indexRes =
+                CameraLiftTriggerSettings.SEARCH_INDEX_DATA_PROVIDER.getXmlResourcesToIndex(
+                    RuntimeEnvironment.application, true /* enabled */);
+
+        assertThat(indexRes).isNotNull();
+        assertThat(indexRes.get(0).xmlResId).isEqualTo(mSettings.getPreferenceScreenResId());
+    }
+}
