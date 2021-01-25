3b82f7ba1cb6ae678497cd9b9a8b1a9f3137c098
Janis Danisevskis <jdanis@google.com>
2021-01-25 14:56:47 -0800

    Keystore 2.0: Update Wifi settings to use mostly public keystore API.
    Test: N/A
    Bug: 171305607
    Bug: 171305388
    Merged-In: Ib794c5f2d904c2b187d7d5fd00b81afc852d0052
    Change-Id: Ib794c5f2d904c2b187d7d5fd00b81afc852d0052

--- tests/robotests/src/com/android/settings/wifi/WifiConfigController2Test.java
+++ tests/robotests/src/com/android/settings/wifi/WifiConfigController2Test.java
@@ -18,7 +18,6 @@ package com.android.settings.wifi;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.robolectric.Shadows.shadowOf;
@@ -31,8 +30,6 @@ import android.net.wifi.WifiEnterpriseConfig;
 import android.net.wifi.WifiEnterpriseConfig.Eap;
 import android.net.wifi.WifiEnterpriseConfig.Phase2;
 import android.net.wifi.WifiManager;
-import android.os.ServiceSpecificException;
-import android.security.KeyStore;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
@@ -74,8 +71,6 @@ public class WifiConfigController2Test {
     private Context mContext;
     @Mock
     private WifiEntry mWifiEntry;
-    @Mock
-    private KeyStore mKeyStore;
     private View mView;
     private Spinner mHiddenSettingsSpinner;
     private ShadowSubscriptionManager mShadowSubscriptionManager;
@@ -270,27 +265,11 @@ public class WifiConfigController2Test {
     }
 
     @Test
-    public void loadCertificates_keyStoreListFail_shouldNotCrash() {
-        // Set up
-        when(mWifiEntry.getSecurity()).thenReturn(WifiEntry.SECURITY_EAP);
-        when(mKeyStore.list(anyString()))
-            .thenThrow(new ServiceSpecificException(-1, "permission error"));
-
-        mController = new TestWifiConfigController2(mConfigUiBase, mView, mWifiEntry,
-              WifiConfigUiBase2.MODE_CONNECT);
-
-        // Verify that the EAP method menu is visible.
-        assertThat(mView.findViewById(R.id.eap).getVisibility()).isEqualTo(View.VISIBLE);
-        // No Crash
-    }
-
-    @Test
     public void loadCertificates_undesiredCertificates_shouldNotLoadUndesiredCertificates() {
         final Spinner spinner = new Spinner(mContext);
-        when(mKeyStore.list(anyString())).thenReturn(WifiConfigController.UNDESIRED_CERTIFICATES);
 
         mController.loadCertificates(spinner,
-                "prefix",
+                Arrays.asList(WifiConfigController.UNDESIRED_CERTIFICATES),
                 "doNotProvideEapUserCertString",
                 false /* showMultipleCerts */,
                 false /* showUsePreinstalledCertOption */);
@@ -418,11 +397,6 @@ public class WifiConfigController2Test {
         @Override
         boolean isSplitSystemUser() {
             return false;
-        }
-
-        @Override
-        KeyStore getKeyStore() {
-            return mKeyStore;
         }
     }
 
