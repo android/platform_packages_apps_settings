22e26f57d30bde979ccc226054547ff037655d0a
Bonian Chen <bonianchen@google.com>
2021-03-20 04:46:39 +0800

    [Settings] Convert to getSystemService(Class<T>)
    Change to getSystemService(Class<T>) to align the capability with framework
    part.
    
    Bug: 179640862
    Test: local
    Change-Id: I1b4812044fc4876bec6645570049a60f9470dbbd

--- tests/robotests/src/com/android/settings/wifi/tether/WifiTetherSSIDPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/wifi/tether/WifiTetherSSIDPreferenceControllerTest.java
@@ -67,8 +67,8 @@ public class WifiTetherSSIDPreferenceControllerTest {
         MockitoAnnotations.initMocks(this);
         mPreference = new WifiTetherSsidPreference(RuntimeEnvironment.application);
 
-        when(mContext.getSystemService(Context.WIFI_SERVICE)).thenReturn(mWifiManager);
-        when(mContext.getSystemService(Context.TETHERING_SERVICE)).thenReturn(mTetheringManager);
+        when(mContext.getSystemService(WifiManager.class)).thenReturn(mWifiManager);
+        when(mContext.getSystemService(TetheringManager.class)).thenReturn(mTetheringManager);
         when(mTetheringManager.getTetherableWifiRegexs()).thenReturn(new String[]{"1", "2"});
         when(mContext.getResources()).thenReturn(RuntimeEnvironment.application.getResources());
         when(mScreen.findPreference(anyString())).thenReturn(mPreference);
