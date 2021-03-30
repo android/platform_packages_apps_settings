2b356a7d2fc3729ebcf7e60306b3b9bb7d3cb63d
Ady Abraham <adyabr@google.com>
2021-03-30 18:25:10 +0000

    Revert "Allow alternative time zone ID to be set device's zone."
    Revert submission 1652476-settings-alternative-ids
    
    Reason for revert: b/183995511
    Reverted Changes:
    I4589f439c:Add TimeZoneMapping#isShownInPickerAfter method.
    I168fb4319:Allow alternative time zone ID to be set device's ...
    
    Change-Id: If4ba9414fc833f44eb78136edaf770c71e92772e

--- src/com/android/settings/datetime/timezone/model/TimeZoneData.java
+++ src/com/android/settings/datetime/timezone/model/TimeZoneData.java
@@ -71,7 +71,7 @@ public class TimeZoneData {
         Set<String> regionIds = new ArraySet<>();
         for (CountryTimeZones countryTimeZone : countryTimeZones) {
             FilteredCountryTimeZones filteredZones = new FilteredCountryTimeZones(countryTimeZone);
-            if (filteredZones.matches(tzId)) {
+            if (filteredZones.getTimeZoneIds().contains(tzId)) {
                 regionIds.add(filteredZones.getRegionId());
             }
         }
